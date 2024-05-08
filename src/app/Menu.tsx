7c0db3f24bc2b36bf6f3ff5eba6fb3135e2fd1ba
matin <matinsadegi3290@gmail.com>
2024-05-08 08:29:09 +0330

    add menu component

---
+++ src/app/Menu.tsx
@@ -0,0 +1,184 @@
+import React from "react";
+import { NastaligFont } from "../utils/fronts";
+
+const Menu = () => {
+  return (
+    <div className=" relative z-40 ">
+      <div className=" bg-menu-background  bg-cover h-[80vh] bg-center text-center py-16">
+        <h2 className={`${NastaligFont.className} text-white `}>
+          منوی گراف
+        </h2>
+        <div className="flex w-[1000px] mx-auto justify-between mt-16">
+          <div className="flex flex-col w-52 space-y-8">
+            <div >
+              <div className="flex items-center justify-between">
+                <h4 className={` text-white  ${NastaligFont.className}`}>
+                  اسپرسو
+                </h4>
+                <p
+                  className={`text-primary-600 text-lg ${NastaligFont.className}`}
+                >
+                  50.000 ت
+                </p>
+              </div>
+              <p className=" text-gray-500 text-right">اسپرسو و کرما</p>
+            </div>
+            <div >
+              <div className="flex items-center justify-between">
+                <h4 className={` text-white  ${NastaligFont.className}`}>
+                  لاته
+                </h4>
+                <p
+                  className={`text-primary-600 text-lg ${NastaligFont.className}`}
+                >
+                  50.000 ت
+                </p>
+              </div>
+              <p className=" text-gray-500 text-right">اسپرسو و کرما</p>
+            </div>
+            <div >
+              <div className="flex items-center justify-between">
+                <h4 className={` text-white  ${NastaligFont.className}`}>
+                  موکا
+                </h4>
+                <p
+                  className={`text-primary-600 text-lg ${NastaligFont.className}`}
+                >
+                  50.000 ت
+                </p>
+              </div>
+              <p className=" text-gray-500 text-right">اسپرسو و کرما</p>
+            </div>
+            <div >
+              <div className="flex items-center justify-between">
+                <h4 className={` text-white  ${NastaligFont.className}`}>
+                  کاپوچینو
+                </h4>
+                <p
+                  className={`text-primary-600 text-lg ${NastaligFont.className}`}
+                >
+                  50.000 ت
+                </p>
+              </div>
+              <p className=" text-gray-500 text-right">اسپرسو و کرما</p>
+            </div>
+            
+          </div>
+          <div className="flex flex-col w-52 space-y-8">
+            <div >
+              <div className="flex items-center justify-between">
+                <h4 className={` text-white  ${NastaligFont.className}`}>
+                  شیک شکلات
+                </h4>
+                <p
+                  className={`text-primary-600 text-lg ${NastaligFont.className}`}
+                >
+                  50.000 ت
+                </p>
+              </div>
+              <p className=" text-gray-500 text-right">اسپرسو و کرما</p>
+            </div>
+            <div >
+              <div className="flex items-center justify-between">
+                <h4 className={` text-white  ${NastaligFont.className}`}>
+                  شیک نوتلا
+                </h4>
+                <p
+                  className={`text-primary-600 text-lg ${NastaligFont.className}`}
+                >
+                  50.000 ت
+                </p>
+              </div>
+              <p className=" text-gray-500 text-right">اسپرسو و کرما</p>
+            </div>
+            <div >
+              <div className="flex items-center justify-between">
+                <h4 className={` text-white  ${NastaligFont.className}`}>
+                  شیرموز
+                </h4>
+                <p
+                  className={`text-primary-600 text-lg ${NastaligFont.className}`}
+                >
+                  50.000 ت
+                </p>
+              </div>
+              <p className=" text-gray-500 text-right">اسپرسو و کرما</p>
+            </div>
+            <div >
+              <div className="flex items-center justify-between">
+                <h4 className={` text-white  ${NastaligFont.className}`}>
+                  معجون
+                </h4>
+                <p
+                  className={`text-primary-600 text-lg ${NastaligFont.className}`}
+                >
+                  50.000 ت
+                </p>
+              </div>
+              <p className=" text-gray-500 text-right">اسپرسو و کرما</p>
+            </div>
+            
+          </div>
+          <div className="flex flex-col w-52 space-y-8">
+            <div >
+              <div className="flex items-center justify-between">
+                <h4 className={` text-white  ${NastaligFont.className}`}>
+                  هات چاکلت
+                </h4>
+                <p
+                  className={`text-primary-600 text-lg ${NastaligFont.className}`}
+                >
+                  50.000 ت
+                </p>
+              </div>
+              <p className=" text-gray-500 text-right">اسپرسو و کرما</p>
+            </div>
+            <div >
+              <div className="flex items-center justify-between">
+                <h4 className={` text-white  ${NastaligFont.className}`}>
+                  هات نوتلا
+                </h4>
+                <p
+                  className={`text-primary-600 text-lg ${NastaligFont.className}`}
+                >
+                  50.000 ت
+                </p>
+              </div>
+              <p className=" text-gray-500 text-right">اسپرسو و کرما</p>
+            </div>
+            <div >
+              <div className="flex items-center justify-between">
+                <h4 className={` text-white  ${NastaligFont.className}`}>
+                  چای ماسالا
+                </h4>
+                <p
+                  className={`text-primary-600 text-lg ${NastaligFont.className}`}
+                >
+                  50.000 ت
+                </p>
+              </div>
+              <p className=" text-gray-500 text-right">اسپرسو و کرما</p>
+            </div>
+            <div >
+              <div className="flex items-center justify-between">
+                <h4 className={` text-white  ${NastaligFont.className}`}>
+                  دمنوش
+                </h4>
+                <p
+                  className={`text-primary-600 text-lg ${NastaligFont.className}`}
+                >
+                  50.000 ت
+                </p>
+              </div>
+              <p className=" text-gray-500 text-right">اسپرسو و کرما</p>
+            </div>
+            
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+ 
+export default Menu;
