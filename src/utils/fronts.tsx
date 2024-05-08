914518d30e6025054771bf29e7bf87cca5840f4d
matin <matinsadegi3290@gmail.com>
2024-05-08 08:23:52 +0330

    add new font and images

---
+++ src/utils/fronts.tsx
@@ -0,0 +1,12 @@
+import localFont from "next/font/local";
+
+export const VazirFont = localFont({
+  src: "../../public/fonts/Vazir.ttf",
+});
+
+export const NastaligFont = localFont({
+  src: "../../public/fonts/IranNastaliq.ttf",
+});
+export const BNazaninFont = localFont({
+  src: "../../public/fonts/B-NAZANIN.ttf",
+});
