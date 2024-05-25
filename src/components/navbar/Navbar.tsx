5805b3b40abcb42da574b0e74e9b934740722452
matin <matinsadegi3290@gmail.com>
2024-05-25 18:14:06 +0330

    responsive navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,36 +1,76 @@
 "use client";
+import { useState } from "react";
 import Image from "next/image";
+import Links from "./links/Links";
+import { usePathname } from "next/navigation";
+import { NastaligFont } from "@/src/utils/fronts";
 import logo from "@/public/images/coffee-icon-png-13692.png";
 import searchIcon from "@/public/icons/search-svgrepo-com (1).svg";
 import searchIcon2 from "@/public/icons/search-night.svg";
 import cartIcon from "@/public/icons/cart-1-svgrepo-com.svg";
 import cartIcon2 from "@/public/icons/cart-night.svg";
-import Links from "./links/Links";
-import { usePathname } from "next/navigation";
+import hamburgerIcon from "@/public/icons/hamburger-4-svgrepo-com.svg";
+import multiplyIcon from "@/public/icons/multiply-svgrepo-com(1).svg";
 
 const Navbar = () => {
   const pathName = usePathname();
+  const [showNavbar, setShowNavbar] = useState(false);
   return (
-    <div
-      className={`py-4 absolute  w-full ${
+    <nav
+      className={` py-5  absolute  w-full ${
         pathName === "/" ? " bg-transparent" : " bg-brown-700"
       }`}
     >
-      <div className=" max-w-[1200px] flex justify-between items-center text-black mx-auto ">
-        <div className="flex items-center">
-          <Image src={logo} alt="logo" width={50} height={30} />
-          <h3 className={`${pathName === "/" ? " text-black" : " text-white"}`}>
-            گراف
-          </h3>
+      <div className=" max-w-[1300px]  flex justify-between items-center text-black mx-auto px-8">
+        <div className=" flex items-center lg:w-1/4 w-full justify-between ">
+          <Image
+            src={hamburgerIcon}
+            alt="hamburger-icon"
+            className=" lg:hidden"
+            onClick={() => setShowNavbar(true)}
+          />
+          <div className="flex items-center">
+            <Image src={logo} alt="logo" width={50} height={30} />
+            <p
+              className={`${pathName === "/" ? " text-black" : " text-white"} ${
+                NastaligFont.className
+              } text-xl pt-3`}
+            >
+              گراف
+            </p>
+          </div>
         </div>
-        <Links />
-        <div className="flex gap-5">
-          <Image src={pathName === '/' ? searchIcon : searchIcon2} alt="search-icon" />
-          <Image src={pathName === '/' ? cartIcon : cartIcon2} alt="cart-icon" /> 
-          <button className=" buttons">ثبت نام</button>
+        <div
+          className={` flex flex-col bg-white z-50 justify-between py-28 items-center top-0 right-0 fixed h-screen w-[350px]  lg:static lg:flex-row lg:bg-transparent ${
+            showNavbar ? "translate-x-0" : "translate-x-full"
+          } lg:translate-x-0 transition-all duration-500 lg:static lg:h-fit lg:py-0 lg:w-full`}
+        >
+          <Image
+            src={multiplyIcon}
+            alt="multiply-icon"
+            className=" lg:hidden absolute top-4 right-4"
+            onClick={() => setShowNavbar(false)}
+          />
+          <Links showNavbar={showNavbar} />
+          <div className="flex lg:justify-end gap-5 order-first lg:order-last lg:w-1/3 ">
+            <Image
+              src={pathName === "/" ? searchIcon : searchIcon2}
+              alt="search-icon"
+            />
+            <Image
+              src={pathName === "/" ? cartIcon : cartIcon2}
+              alt="cart-icon"
+            />
+            <button className=" buttons">ثبت نام</button>
+          </div>
         </div>
       </div>
-    </div>
+      <div
+        className={`lg:hidden fixed h-screen top-0 w-screen bg-[rgba(0,0,0,0.5)] z-40 ${
+          showNavbar ? "block" : "hidden"
+        }`}
+      ></div>
+    </nav>
   );
 };
 
--- src/components/navbar/links/Links.tsx
+++ src/components/navbar/links/Links.tsx
@@ -29,7 +29,7 @@ const links = [
 const Links = () => {
   const pathName = usePathname();
   return (
-    <div className="flex gap-6">
+    <div className="flex lg:w-2/3  h-[450px] lg:px-20 xl:px-28  w-full  justify-between items-center flex-col lg:flex-row lg:h-fit">
       {links.map((link) => (
         <Link
           href={link.path}
