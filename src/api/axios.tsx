edec8341f4b4a9aafd172672b22fb060ef031c62
Matin Sadeghi <matinsadegi3290@gmail.com>
2024-06-30 18:11:32 +0330

    create axios interceptors

---
+++ src/api/axios.tsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+const BASE_URL = "http://localhost:5000";
+
+export const app = axios.create({ baseURL: BASE_URL, withCredentials: true });
+
+
+app.interceptors.request.use(
+  (res) => res,
+  (err) => Promise.reject(err)
+); 
+app.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const originalConfig = err?.config;
+    if (err.response?.status === 401 && !originalConfig?._retry) {
+      originalConfig._retry = true;
+      try {
+        const { data } = await axios.get(`${BASE_URL}/user/refresh`, {
+          withCredentials: true,
+        }); 
+     
+        if (data) return axios(originalConfig);
+      } catch (error) {
+      return Promise.reject(error);
+      }
+    }
+    return Promise.reject(err);
+  }
+);
