b36fe8d3504e3c8ee371d151ec0f8e0e6458a8af
Matin Sadeghi <matinsadegi3290@gmail.com>
2024-07-02 16:46:52 +0330

    fix spinner bugs

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PageLoadingSpinner from "../components/spinner/PageLoadingSpinner";
+
+const Loading = () => {
+  return <div><PageLoadingSpinner/></div>;
+};
+
+export default Loading;
---
+++ src/components/spinner/PageLoadingSpinner.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./PageLoadingSpinner.css";
+
+const PageLoadingSpinner = () => {
+  return (
+    <div className=" flex h-screen w-screen bg-white fixed z-20 items-center justify-center ">
+      <span className="loader2"></span>
+    </div>
+  );
+}
+
+export default PageLoadingSpinner
