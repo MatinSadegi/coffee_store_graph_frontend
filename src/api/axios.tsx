a4f6d9306c78dd56f4d8a3871f0a3a44cda60905
Matin Sadeghi <matinsadegi3290@gmail.com>
2024-07-04 12:36:29 +0330

    wrap query client provider

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,13 +1,12 @@
 import axios from "axios";
-const BASE_URL = "https://coffee-store-graph-backend.onrender.com/";
+const BASE_URL = "http://localhost:5000/";
 
 export const app = axios.create({ baseURL: BASE_URL, withCredentials: true });
-
 
 app.interceptors.request.use(
   (res) => res,
   (err) => Promise.reject(err)
-); 
+);
 app.interceptors.response.use(
   (res) => res,
   async (err) => {
@@ -17,11 +16,11 @@ app.interceptors.response.use(
       try {
         const { data } = await axios.get(`${BASE_URL}/user/refresh`, {
           withCredentials: true,
-        }); 
-     
+        });
+
         if (data) return axios(originalConfig);
       } catch (error) {
-      return Promise.reject(error);
+        return Promise.reject(error);
       }
     }
     return Promise.reject(err);
